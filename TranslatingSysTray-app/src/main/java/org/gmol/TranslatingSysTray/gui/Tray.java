87f09451333402edd6959c634dee0ae282d4be9a
Gmol <arthur.gmol@gmail.com>
2012-11-14 11:44:40 +0000

    Minor modifications

--- TranslatingSysTray-app/src/main/java/org/gmol/TranslatingSysTray/gui/Tray.java
+++ TranslatingSysTray-app/src/main/java/org/gmol/TranslatingSysTray/gui/Tray.java
@@ -15,49 +15,53 @@ import java.awt.event.ActionListener;
  * 
  */
 public class Tray {
-	
-    private static final String TOOLTIP = "tray test";
-    private static final String IMAGE = "/usr/share/pixmaps/gnome-gmush.png";
-    private static PopupMenu menu;
 
-    public static void main(String[] args) {
-//        new Tray().start();
-    }
+	private static final String TOOLTIP = "tray test";
+	private static final String IMAGE = "/usr/share/pixmaps/gnome-gmush.png";
 
-    private void start() {
-        if (SystemTray.isSupported()) {
-            SystemTray tray = SystemTray.getSystemTray();
-            Image trayImage = Toolkit.getDefaultToolkit().getImage(IMAGE);
-            createMenu();
-            TrayIcon trayIcon = new TrayIcon(trayImage, TOOLTIP, menu);
-            trayIcon.setImageAutoSize(true);
-            try {
-                tray.add(trayIcon);
-                trayIcon.displayMessage("caption", "howdy", TrayIcon.MessageType.INFO);
-            } catch (AWTException e) {
-                System.err.println("Error starting tray: " + e);
-            }
+	public Tray() {
+		open();
+	}
 
-        } else {
-            System.err.println("SystemTray not supported");
-        }
-    }
+	public static void main(String[] args) {
+		// new Tray().open();
+	}
 
-    private void createMenu() {
-        menu = new PopupMenu();
-        MenuItem foo = new MenuItem("foo");
-        MenuItem exit = new MenuItem("exit");
-        exit.addActionListener(new exitListener());
-        menu.add(foo);
-        menu.addSeparator();
-        menu.add(exit);
-    }
+	private void open() {
+		if (SystemTray.isSupported()) {
+			SystemTray tray = SystemTray.getSystemTray();
+			Image trayImage = Toolkit.getDefaultToolkit().getImage(IMAGE);
+			TrayIcon trayIcon = new TrayIcon(trayImage, TOOLTIP, createMenu());
+			trayIcon.setImageAutoSize(true);
+			try {
+				tray.add(trayIcon);
+				trayIcon.displayMessage("caption", "howdy",
+						TrayIcon.MessageType.INFO);
+			} catch (AWTException e) {
+				System.err.println("Error starting tray: " + e);
+			}
 
-    class exitListener implements ActionListener {
+		} else {
+			System.err.println("SystemTray not supported");
+		}
+	}
 
-        public void actionPerformed(ActionEvent e) {
-            System.out.println("exiting...");
-            System.exit(0);
-        }
-    }
-}  
+	private PopupMenu createMenu() {
+		PopupMenu menu = new PopupMenu();
+		MenuItem foo = new MenuItem("foo");
+		MenuItem exit = new MenuItem("exit");
+		exit.addActionListener(new exitListener());
+		menu.add(foo);
+		menu.addSeparator();
+		menu.add(exit);
+		return menu;
+	}
+
+	class exitListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("exiting...");
+			System.exit(0);
+		}
+	}
+}
