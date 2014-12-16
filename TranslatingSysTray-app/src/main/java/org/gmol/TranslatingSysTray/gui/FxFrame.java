68e309990cb1d71eaeba5e9a02616f7c45db4872
Gmol <arthur.gmol@gmail.com>
2014-12-16 17:07:16 +0000

    Implementing mouse handling in FxFrame

--- TranslatingSysTray-app/src/main/java/org/gmol/TranslatingSysTray/gui/FxFrame.java
+++ TranslatingSysTray-app/src/main/java/org/gmol/TranslatingSysTray/gui/FxFrame.java
@@ -46,6 +46,7 @@ public class FxFrame extends JFrame implements IFrame {
 
 	private static final Logger LOGGER = Logger.getLogger(FxFrame.class);
 	private String translation = "";
+	public MouseListener trayMouseListener = null;
 
 	public FxFrame() {
 		// TODO Auto-generated constructor stub
@@ -92,7 +93,7 @@ public class FxFrame extends JFrame implements IFrame {
 		String content = meta + css + rest;
 		LOGGER.debug("index of meta data: " + metaindex);
 		LOGGER.debug("content:\n" + content);
-		Scene scene = new Scene(new Browser(content), 800, 600, Color.web("#666970"));
+		Scene scene = new Scene(new Browser(this, content), 800, 600, Color.web("#666970"));
 		// Text text = new Text();
 		// text.setX(40);
 		// text.setY(100);
@@ -112,8 +113,8 @@ public class FxFrame extends JFrame implements IFrame {
 
 	// @Override
 	public void addMouseListener(MouseListener l) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub	
+	    this.trayMouseListener = l;    
 	}
 
 	// @Override
@@ -153,6 +154,7 @@ class Browser extends Region {
 
 	final WebView browser = new WebView();
 	final WebEngine webEngine = browser.getEngine();
+	private FxFrame frame = null;
 	private static final Logger LOGGER = Logger.getLogger(Browser.class);
 
 	private static String readFile(String path) throws IOException {
@@ -169,7 +171,8 @@ class Browser extends Region {
 		}
 	}
 
-	public Browser(String translation) {
+	public Browser(FxFrame frame, String translation) {
+	    this.frame = frame;
 		// apply the styles
 		// getStyleClass().add("browser");
 		// load the web page
@@ -189,10 +192,13 @@ class Browser extends Region {
 		}
 		// add the web view to the scene
 		browser.setContextMenuEnabled(false);
-		browser.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
-	    	 
+		browser.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {	    	 
 	        @Override
 	        public void handle(javafx.scene.input.MouseEvent mouseEvent) {
+	            
+	            java.awt.event.MouseEvent me = createAwtMouseEvent(mouseEvent);
+	            Browser.this.frame.trayMouseListener.mouseClicked(me);
+	            
 	        	MouseButton button = mouseEvent.getButton();
 	        	
 	        	if (button.equals(MouseButton.PRIMARY)) {
@@ -236,5 +242,58 @@ class Browser extends Region {
 	@Override
 	protected double computePrefHeight(double width) {
 		return 500;
+	}
+	
+	private java.awt.event.MouseEvent createAwtMouseEvent(javafx.scene.input.MouseEvent mouseEvent) {
+	    int id = -1;
+	    switch (mouseEvent.getEventType()) {
+        case javafx.scene.input.MouseEvent.MOUSE_CLICKED:
+            id = java.awt.event.MouseEvent.MOUSE_CLICKED; 
+            break;
+        case javafx.scene.input.MouseEvent.MOUSE_PRESSED:
+            id = java.awt.event.MouseEvent.MOUSE_PRESSED;
+            break;
+        case javafx.scene.input.MouseEvent.MOUSE_RELEASED:
+            id = java.awt.event.MouseEvent.MOUSE_RELEASED;
+            break;
+        case javafx.scene.input.MouseEvent.MOUSE_ENTERED:
+            id = java.awt.event.MouseEvent.MOUSE_ENTERED;
+            break;
+        case javafx.scene.input.MouseEvent.MOUSE_EXITED:
+            id = java.awt.event.MouseEvent.MOUSE_EXITED;
+            break;
+        case javafx.scene.input.MouseEvent.MOUSE_MOVED:
+            id = java.awt.event.MouseEvent.MOUSE_MOVED;
+            break;  
+        default:
+            LOGGER.warn("Event[" + mouseEvent.getEventType().toString() + "] not supported");
+            break;
+        }
+	    
+	    
+	    int modifiers = -1;
+	    int button = -1;
+	    boolean popupTrigger = false;
+	    switch (mouseEvent.getButton()) {
+        case javafx.scene.input.MouseButton.PRIMARY:
+            modifiers = java.awt.event.MouseEvent.BUTTON1_MASK;
+            button = java.awt.event.MouseEvent.BUTTON1 ;
+            break;
+        case javafx.scene.input.MouseButton.SECONDARY:
+            modifiers = java.awt.event.MouseEvent.BUTTON2_MASK;
+            button = java.awt.event.MouseEvent.BUTTON2 ;
+            break;
+        case javafx.scene.input.MouseButton.MIDDLE:
+            modifiers = java.awt.event.MouseEvent.BUTTON3_MASK;
+            button = java.awt.event.MouseEvent.BUTTON3 ;
+            break;
+        case javafx.scene.input.MouseButton.NONE:
+            button = java.awt.event.MouseEvent.NOBUTTON;
+            break;
+        default:
+            LOGGER.warn("Mouse button[" + mouseEvent.getButton().toString() + "] not supported");
+            break;
+        }
+	    return new java.awt.event.MouseEvent(null, id, (new java.util.Date()).getTime(), modifiers, (int)mouseEvent.getX(), (int)mouseEvent.getY(), (int)mouseEvent.getScreenX(), (int)mouseEvent.getScreenY(), mouseEvent.getClickCount(), popupTrigger, button);
 	}
 }
